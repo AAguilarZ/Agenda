e8bda9247ec7fc3ce207b289a45431d6a935ae77
AAguilarZ <alfonsoaguilarzambo@gmail.com>
2023-07-31 09:57:46 +0100

    Mi primera version

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using AgendaContactos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaContactos.Datos
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { 
+
+        }
+        // agregar los modelos aqui
+        public DbSet<Contacto> Contacto { get; set; }
+
+    }
+}
---
+++ Models/Contacto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaContactos.Models
+{
+    public class Contacto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este dato es requerido")]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "Este dato es requerido")]
+        public string Apellido1 { get; set; }
+
+        [Required(ErrorMessage = "Este dato es requerido")]
+        public string Apellido2 { get; set; }
+
+        [Required(ErrorMessage = "Este dato es requerido")]
+        public string Telefono { get; set; }
+
+        [Required(ErrorMessage = "Este dato es requerido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Este dato es requerido")]
+        public DateTime FechaNacimiento { get; set; }
+
+
+    }
+}
